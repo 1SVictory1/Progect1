9a5bf0b06c9130f3646de5d3bea7bf9095f3afb4
Александр Яковлев <alek2002sandr@gmail.com>
2021-07-05 15:30:02 +0300

    g

--- Progect/Form1.cs
+++ Progect/Form1.cs
@@ -38,65 +38,70 @@ namespace Progect
             return token;
         }
 
-        // получить токен из файла
-        //public static string getAuthForUser()
-        //{
-        //    string fileName = @"token.txt";
-        //    string token = "";
-        //    try
-        //    {
-        //        using (StreamReader sr = new StreamReader(fileName))
-        //        {
-        //            sr.ReadLine();
-        //            token = sr.ReadLine();
-        //        }
-        //    }
-        //    catch (Exception e)
-        //    {
-        //        Console.WriteLine(e.Message);
-        //    }
-        //    return token;
-        //}
+        private void Form1_Load(object sender, EventArgs e)
+        {
 
-        //static void Main(string[] args)
-        //{
+        }
 
-        //    var api_group = new VkApi();
-        //    // обработать исключения!
-        //    api_group.Authorize(new ApiAuthParams
-        //    {
-        //        AccessToken = getAuthForGroup()
-        //    });
+        //получить токен из файла
+        public static string getAuthForUser()
+        {
+            string fileName = @"token.txt";
+            string token = "";
+            try
+            {
+                using (StreamReader sr = new StreamReader(fileName))
+                {
+                    sr.ReadLine();
+                    token = sr.ReadLine();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return token;
+        }
 
-        //    // получит запись со стены (для пользователя)
-        //    var api_user = new VkApi();
-        //    // обработать исключения!
-        //    api_user.Authorize(new ApiAuthParams
-        //    {
-        //        AccessToken = getAuthForUser()
-        //    });
+        static void Main(string[] args)
+        {
+            var api_group = new VkApi();
+            // обработать исключения!
+            api_group.Authorize(new ApiAuthParams
+            {
+                AccessToken = getAuthForGroup()
+            });
 
-        //    // получить список подписчиков сообщества (для сообщества)
-        //    var getFollowers = api_group.Groups.GetMembers(new GroupsGetMembersParams()
-        //    {
-        //        GroupId = "205575031",
-        //        Fields = VkNet.Enums.Filters.UsersFields.FirstNameAbl
-        //    });
-        //    foreach (User user in getFollowers)
-        //        Console.WriteLine(user.FirstName);
+            // получит запись со стены (для пользователя)
+            var api_user = new VkApi();
+            // обработать исключения!
+            api_user.Authorize(new ApiAuthParams
+            {
+                AccessToken = getAuthForUser()
+            });
 
-            // получить список друзей (для пользователя)
-            //var getFriends = api_user.Friends.Get(new VkNet.Model.RequestParams.FriendsGetParams
-            //{
-            //    Fields = VkNet.Enums.Filters.ProfileFields.All
-            //});
-            //foreach (User user in getFriends)
-            //    Console.WriteLine(user.FirstName);
+            // получить список подписчиков сообщества (для сообщества)
+            var getFollowers = api_group.Groups.GetMembers(new GroupsGetMembersParams()
+            {
+                GroupId = "205575031",
+                Fields = VkNet.Enums.Filters.UsersFields.FirstNameAbl
+            });
+            foreach (User user in getFollowers)
+                Console.WriteLine(user.FirstName);
+
+            // получить список друзей(для пользователя)
+            var getFriends = api_user.Friends.Get(new VkNet.Model.RequestParams.FriendsGetParams
+            {
+                Fields = VkNet.Enums.Filters.ProfileFields.All
+            });
+            foreach (User user in getFriends)
+                Console.WriteLine(user.FirstName);
 
 
             //var get = api_user.Wall.Get(new WallGetParams());
             //foreach (var wallPosts in get.WallPosts)
             //    Console.WriteLine(Encoding.Default.GetString(Encoding.UTF8.GetBytes(wallPosts.Text)));
-        //}
+            
+        }
     }
 }
