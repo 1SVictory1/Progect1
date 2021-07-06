d38f6199aab27f62a816a1ad566ab6ddfffe6641
Kirillkuk <khryavin@list.ru>
2021-07-06 21:17:11 +0300

    Форма 1 изменение стиля

--- Progect/Form1.Designer.cs
+++ Progect/Form1.Designer.cs
@@ -65,7 +65,7 @@ namespace Progect
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(67, 176);
+            this.label1.Location = new System.Drawing.Point(2, 176);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(332, 37);
             this.label1.TabIndex = 5;
@@ -75,7 +75,7 @@ namespace Progect
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(95, 252);
+            this.label2.Location = new System.Drawing.Point(39, 261);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(295, 37);
             this.label2.TabIndex = 6;
@@ -83,25 +83,25 @@ namespace Progect
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(457, 176);
+            this.textBox1.Location = new System.Drawing.Point(417, 186);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(345, 37);
+            this.textBox1.Size = new System.Drawing.Size(679, 27);
             this.textBox1.TabIndex = 7;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(457, 252);
+            this.textBox2.Location = new System.Drawing.Point(417, 270);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(345, 37);
+            this.textBox2.Size = new System.Drawing.Size(679, 28);
             this.textBox2.TabIndex = 8;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(825, 692);
+            this.ClientSize = new System.Drawing.Size(1178, 692);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label2);
@@ -110,6 +110,7 @@ namespace Progect
             this.Controls.Add(this.button1);
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "API VK";
             this.ResumeLayout(false);
             this.PerformLayout();
